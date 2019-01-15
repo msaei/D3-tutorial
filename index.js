c6ceeadf584b215347e84bdd3bb0fa0724dae20d
Mehdi M. Saei <mahdi.saeikhalili@gmail.com>
2019-01-15 14:48:27 -0500

    group elements and transform group

--- index.js
+++ index.js
@@ -3,24 +3,25 @@ const canvas = d3.select('.canvas');
 const svg = canvas.append('svg')
     .attr('width', 700)
     .attr('height', 700);
-
+const group = svg.append('g')
+    .attr('transform', 'translate(0, 100)');
 // add shapes to the svg container
-svg.append('rect')
+group.append('rect')
     .attr('width', 100)
     .attr('height', 50)
     .attr('x', 20)
     .attr('y', 20)
     .attr('fill', 'blue');
-svg.append('circle')
+group.append('circle')
     .attr('r', 50)
     .attr('cx', 200)
     .attr('cy', 50)
     .attr('fill', 'pink');
-svg.append('line')
-    .attr('x1', 50)
-    .attr('y1', 250)
-    .attr('x2', 100)
-    .attr('y2', 200)
+group.append('line')
+    .attr('x1', 280)
+    .attr('y1', 20)
+    .attr('x2', 300)
+    .attr('y2', 100)
     .attr('stroke', 'red');
 
 svg.append('text')
