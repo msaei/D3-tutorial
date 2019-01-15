acecffe539abf243d5d633959412f4a33427ad96
Mehdi M. Saei <mahdi.saeikhalili@gmail.com>
2019-01-15 14:31:14 -0500

    append elements to svg

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-const canvas = d3.select('.canvas')
+const canvas = d3.select('.canvas');
+
+const svg = canvas.append('svg');
+
+// add shapes to the svg container
+svg.append('rect');
+svg.append('circle');
+svg.append('line');
