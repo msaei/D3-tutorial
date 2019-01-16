bef063f58fc32e1d1062a525cea469354552f10c
Mehdi M. Saei <mahdi.saeikhalili@gmail.com>
2019-01-16 15:30:31 -0500

    bandScale function used to scale a chart

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 // select svg container first
 const svg = d3.select('svg');
 
-const y = d3.scaleLinear()
-    .domain([0, 1000])
-    .range([0, 500]);
+
 
 d3.json('menu.json').then(data => {
+    // scaleing x , y
+    const y = d3.scaleLinear()
+        .domain([0, 1000])
+        .range([0, 500]);
+    const x = d3.scaleBand()
+        .domain(data.map(item => item.name))
+        .range([0, 500])
+        .paddingInner(0.1)
+        .paddingOuter(0.2);
+
     // join data to circles group
     const rects = svg.selectAll('rect').data(data);
 
     // add attrs to circles already are in DOM
-    rects.attr('width', 50)
+    rects.attr('width', x.bandwidth)
         .attr('height', d => y(d.orders))
         .attr('fill', 'orange')
-        .attr('x', (d, i) => i * 60);
+        .attr('x', d => x(d.name));
 
     // append enter selection to DOM and add attrs to them
     rects.enter().append('rect')
-        .attr('width', 50)
+        .attr('width', x.bandwidth)
         .attr('height', d => y(d.orders))
         .attr('fill', 'orange')
-        .attr('x', (d, i) => i * 60);
+        .attr('x', d => x(d.name));
 });
