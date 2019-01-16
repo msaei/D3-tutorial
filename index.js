b4a7513decb1602a82b4a8cc5a8a8d9704af4de4
Mehdi M. Saei <mahdi.saeikhalili@gmail.com>
2019-01-15 23:59:20 -0500

    using enter function to add elements to dom

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const data = [
-    { width: 300, height: 150, fill: 'green' }
+    { width: 300, height: 150, fill: 'green' },
+    { width: 200, height: 50, fill: 'pink' },
+    { width: 100, height: 20, fill: 'red' }
 ]
 const svg = d3.select('svg');
 
-svg.select('rect')
-    .data(data)
-    .attr('height', (d, i, n) => {
-        console.log(i)
-        console.log(n)
-        return d.height
-    })
+const rects = svg.selectAll('rect').data(data);
+
+rects.enter().append('rect')
+    .attr('height', d => d.height)
     .attr('width', d => d.width)
-    .attr('fill', d => d.fill)
+    .attr('fill', d => d.fill);
